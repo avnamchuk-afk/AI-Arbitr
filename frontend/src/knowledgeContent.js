cf3265c2cacb05f2282c9d3002438161a7e33fa2
Alex <alex@MacBook-Air.local>
2026-09-21 23:13:31 +0300

    Use working HTTP legal portal link

--- frontend/src/knowledgeContent.js
+++ frontend/src/knowledgeContent.js
@@ -301,7 +301,7 @@ export const knowledgeArticles = [
         ],
       },
     ],
-    sources: [["Федеральный закон № 152-ФЗ «О персональных данных»", "https://ips.pravo.gov.ru/search/98490812b3409e2a8d78a11ca9010f434ea3d9250a11dbbdb78690cd5551bdd6"]],
+    sources: [["Федеральный закон № 152-ФЗ «О персональных данных»", "http://ips.pravo.gov.ru/search/98490812b3409e2a8d78a11ca9010f434ea3d9250a11dbbdb78690cd5551bdd6"]],
     related: ["final-documents", "electronic-signature", "free-access"],
   },
   {
